0df183d1ddb69188b38ef56beb3184ea9bf158bd
cjr61 <cjr61@zips.uakron.edu>
2019-02-13 18:23:23 -0500

    made  a 2000 length random gene generator

--- PCR.py
+++ PCR.py
@@ -1,2 +1,12 @@
 # A PCR program for Dr. Duan's bioinformatics class
 # Written by Cameron Reilly and Tristen Hess, copyright 2019
+
+import random
+import string
+
+bPairs = "actg"
+gene = ""
+for i in range(2000):
+    gene +=random.choice(bPairs)
+
+print(gene)
