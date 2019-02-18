9473e40838fd2c6ac2c2744cd7183f2ed9fc4f7e
cjr61 <cjr61@zips.uakron.edu>
2019-02-18 12:06:36 -0500

    created second DNA half

--- PCR.py
+++ PCR.py
@@ -6,14 +6,33 @@ import string
 import re
 
 bPairs = "actg"
-gene = ""
-for i in range(2000):
-    gene +=random.choice(bPairs)
+geneHalf1 = ""
+geneHalf2 = ""
 
-print(gene)
-regEx = "atg.{197}"
-m = re.search(regEx, gene)
-amplify = m.group(0)
-print (len(amplify))
-print (amplify)
+# randomly generate half a DNA sequence
+for i in range(2000):
+    geneHalf1 +=random.choice(bPairs)
+
+# create the other half of the DNA sequence
+for letter in geneHalf1:
+    if letter == 'a':
+        geneHalf2 += 't'
+    if letter == 't':
+        geneHalf2 += 'a'
+    if letter == 'c':
+        geneHalf2 += 'g'
+    if letter == 'g':
+        geneHalf2 += 'c'
+
+# Reverse second half so it reads from 3' to 5'
+reverseGene = "".join(reversed(geneHalf2))
+print (geneHalf1)
+print (geneHalf2)
+
+# print(gene)
+# regEx = "atg.{197}"
+# m = re.search(regEx, gene)
+# amplify = m.group(0)
+# print (len(amplify))
+# print (amplify)
 
